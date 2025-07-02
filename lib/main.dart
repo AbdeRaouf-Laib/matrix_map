ca5b81d0009e9e15755183d51c6376129a87d58a
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2025-07-02 15:49:51 +0100

    dza

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Google Maps Demo',
-      home: MapScreen(),
+      home: Page(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                  child: Text(
+                    "New",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                  child: Text(
+                    "Obtine",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -28,7 +82,7 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
   final LatLng _initialPosition = const LatLng(0, 0); // Default position
-  Set<Marker> _markers = {};
+  final Set<Marker> _markers = {};
   bool _isLoading = true;
 
   @override
@@ -46,51 +100,51 @@ class _MapScreenState extends State<MapScreen> {
 
     // if (response.statusCode == 200) {
     //   final List<dynamic> data = json.decode(response.body);
-    final List data = [
-      {
-        "markerId": 4,
-        "latlag": LatLng(36.7538, 3.0598),
-        "info": InfoWindow(
-          title: "9ador", // Display the name as title
-          snippet: "ybi3 el batata", // Additional info
-        ),
-      },
-      {
-        "markerId": 3,
-        "latlag": LatLng(36.7540, 3.0578),
-        "info": InfoWindow(
-          title: "bo3lam", // Display the name as title
-          snippet: "ybi3 el besel", // Additional info
-        ),
-      },
-      {
-        "markerId": 1,
-        "latlag": LatLng(36.7539, 3.0588),
-        "info": InfoWindow(
-          title: "3adel", // Display the name as title
-          snippet: "l3ziz lghali ymed free coffes", // Additional info
-        ),
-      },
-      {
-        "markerId": 2,
-        "latlag": LatLng(36.75382, 3.0599),
-        "info": InfoWindow(
-          title: "matrix", // Display the name as title
-          snippet: "local", // Additional info
-        ),
-      },
-    ];
-    setState(() {
-      _markers = data
-          .map(
-            (marker) => Marker(
-              markerId: MarkerId(marker["markerId"].toString()),
-              position: marker["latlag"],
-              infoWindow: marker['info'],
-            ),
-          )
-          .toSet();
-    });
+    // final List data = [
+    //   {
+    //     "markerId": 4,
+    //     "latlag": LatLng(36.7538, 3.0598),
+    //     "info": InfoWindow(
+    //       title: "9ador", // Display the name as title
+    //       snippet: "ybi3 el batata", // Additional info
+    //     ),
+    //   },
+    //   {
+    //     "markerId": 3,
+    //     "latlag": LatLng(36.7540, 3.0578),
+    //     "info": InfoWindow(
+    //       title: "bo3lam", // Display the name as title
+    //       snippet: "ybi3 el besel", // Additional info
+    //     ),
+    //   },
+    //   {
+    //     "markerId": 1,
+    //     "latlag": LatLng(36.7539, 3.0588),
+    //     "info": InfoWindow(
+    //       title: "3adel", // Display the name as title
+    //       snippet: "l3ziz lghali ymed free coffes", // Additional info
+    //     ),
+    //   },
+    //   {
+    //     "markerId": 2,
+    //     "latlag": LatLng(36.75382, 3.0599),
+    //     "info": InfoWindow(
+    //       title: "matrix", // Display the name as title
+    //       snippet: "local", // Additional info
+    //     ),
+    //   },
+    // ];
+    // setState(() {
+    //   _markers = data
+    //       .map(
+    //         (marker) => Marker(
+    //           markerId: MarkerId(marker["markerId"].toString()),
+    //           position: marker["latlag"],
+    //           infoWindow: marker['info'],
+    //         ),
+    //       )
+    //       .toSet();
+    // });
   }
 
   @override
@@ -105,7 +159,7 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           GoogleMap(
-            mapType: MapType.satellite,
+            mapType: MapType.normal,
             onMapCreated: (controller) => mapController = controller,
             initialCameraPosition: CameraPosition(
               target: LatLng(36.7538, 3.0598),
@@ -120,12 +174,7 @@ class _MapScreenState extends State<MapScreen> {
             zoomGesturesEnabled: true,
             zoomControlsEnabled: true,
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              
-            ),
-          ),
+          Container(decoration: BoxDecoration(color: Colors.white)),
         ],
       ),
     );
